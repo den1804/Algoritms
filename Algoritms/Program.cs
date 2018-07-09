46e9ca7987ea9ec57b0fdcddeae11efcee437dba
Підвашецький Денис <pod.den1804@gmail.com>
2018-07-10 01:44:37 +0300

    Create simple list, simple linked list, and start to create linked list

--- Algoritms/Program.cs
+++ Algoritms/Program.cs
@@ -2,11 +2,31 @@
 
 namespace Algoritms
 {
+    class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Node first = new Node { Value = 5 };
+            Node second = new Node { Value = 12 };
+            Node last = new Node { Value = 18 };
+            first.Next = second;
+            second.Next = last;
+            PrintList(first);
+            Console.ReadKey();
+        }
+
+        private static void PrintList(Node node)
+        {
+            while (node != null)
+            {
+                Console.WriteLine(node.Value);
+                node = node.Next;
+            }
         }
     }
 }
---
+++ LinkedListNode/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LinkedListNode
+{
+    public class LinkedListNode<T>
+    {
+        public LinkedListNode (T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; internal set; }
+        public LinkedListNode<T> Next { get; internal set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListNode<string> first = new LinkedListNode<string>("First");
+            LinkedListNode<string> second = new LinkedListNode<string>("Second");
+
+            first.Next = second;
+
+            Console.WriteLine(first.Value);
+            Console.WriteLine(first.Next.Value);
+
+            Console.ReadKey();
+        }
+    }
+}

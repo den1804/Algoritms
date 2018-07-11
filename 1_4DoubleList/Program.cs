61d7c565a28c352ea1f1431650e603853e486549
Denis Pidavashetskyi <pod.den1804@gmail.com>
2018-07-11 17:33:28 +0300

    Готовое решение для односвязных списков и методы добавить вначало и вконец для двухсвзязных списков

---
+++ 1_4DoubleList/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace DoubleList
+{
+    public class LinkedList<T> : IEnumerable<T>
+    {
+        LinkedListNode<T> _head;
+        LinkedListNode<T> _tail;
+        public int Count { get; private set; }
+
+        #region Add First
+        public void AddFirst(T value)
+        {
+            LinkedListNode<T> node = new LinkedListNode<T>(value);
+            LinkedListNode<T> temp = _head;
+            _head = node;
+            _head.Next = temp;
+            if (Count == 0)
+                _tail = _head;
+            else
+                temp.Previous = _head;
+            Count++;
+        }
+        #endregion
+        #region Add Last
+        public void AddLast(T value)
+        {
+            LinkedListNode<T> node = new LinkedListNode<T>(value);
+            if (_tail != null)
+            {
+                _tail.Next = node;
+                node.Previous = _tail;
+            }
+            else
+            {
+                _head = node;
+            }
+            _tail = node;
+            Count++;
+        }
+        #endregion
+        #region Clear 
+        public void Clear()
+        {
+            _head = null;
+            _tail = null;
+            Count = 0;
+        }
+        #endregion
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            LinkedListNode<T> current = _head;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> text = new LinkedList<string> { };
+            text.AddFirst("hello");
+            text.AddFirst("Denis");
+            text.AddLast("!");
+            Display(text);
+            Console.WriteLine(text.Count);
+            text.Clear();
+            Console.WriteLine(text.Count);
+            text.AddLast("Hello");
+            text.AddLast("Denis");
+            text.AddLast("!");
+            Display(text);
+            Console.ReadKey();
+        }
+
+        private static void Display(LinkedList<string> text)
+        {
+            foreach (string str in text)
+                Console.Write(str + " ");
+        }
+    }
+}
--- LinkedListClass/Program.cs
+++ LinkedListClass/Program.cs
@@ -1,12 +1,144 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace LinkedListClass
 {
+    class LinkedList<T> : IEnumerable<T>
+    {
+        public LinkedListNode<T> _head;
+        public LinkedListNode<T> _tail;
+        public int Count { get; private set; }
+
+        #region Add Element
+        public void Add(T value)
+        {
+            LinkedListNode<T> node = new LinkedListNode<T>(value);
+
+            if (_head == null)
+            {
+                _head = node;
+                _tail = node;
+            }
+            else
+            {
+                _tail.Next = node;
+                _tail = node;
+            }
+            Count++;
+        }
+        #endregion
+        #region Remove Element
+        public bool Revome(T item)
+        {
+            LinkedListNode<T> previous = null;
+            LinkedListNode<T> current = _head;
+
+            while (current != null)
+            {
+                if (current.Value.Equals(item))
+                {
+                    if (previous != null)
+                    {
+                        previous.Next = current.Next;
+                        if (previous.Next == null)
+                        {
+                            _tail = previous;
+                        }
+                    }
+                    else
+                    {
+                        _head = _head.Next;
+                        if (_head == null)
+                        {
+                            _tail = null;
+                        }
+                    }
+                    Count--;
+                    return true;
+                }
+                previous = current;
+                current = current.Next;
+            }
+            return false;
+        }
+        #endregion
+        #region Contains
+        public bool Contains(T value)
+        {
+            LinkedListNode<T> current = _head;
+            while (current != null)
+            {
+                if(current.Value.Equals(value))
+                {
+                    return true;
+                }
+                current = current.Next;
+            }
+            return false;
+        }
+        #endregion
+        #region Clear LinkedList
+        public void Clear()
+        {
+            _head = null;
+            _tail = null;
+            Count = 0;
+        }
+        #endregion
+        #region CopyTo
+        public void CopyTo(T[] arr, int arrayIndex)
+        {
+            LinkedListNode<T> current = _head;
+            while (current != null)
+            {
+                arr[arrayIndex++] = current.Value;
+                current = current.Next;
+            }
+        }
+        #endregion
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            LinkedListNode<T> current = _head;
+
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<T>)this).GetEnumerator();
+        }
+    }
     class Program
     {
+        public static void Display(LinkedList<int> list, string text)
+        {
+            Console.WriteLine(text);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            LinkedList<int> instanse = new LinkedList<int> { };
+            instanse.Add(10);
+            instanse.Add(14);
+            instanse.Add(17);
+            instanse.Add(19);
+            Console.WriteLine(instanse.Revome(11));
+            Console.WriteLine("Before remove " + instanse.Contains(19));
+            Console.WriteLine(instanse.Revome(19));
+            Console.WriteLine("After remove " + instanse.Contains(19));
+            Display(instanse, "Adds");
+            instanse.Clear();
+            Display(instanse, "Cleared");
+            Console.ReadKey();
         }
     }
 }
